17eb1100ab6d94c6bda6433af302dea97c31d1cf
balfoldi <balfoldi@yahoo.fr>
2020-11-04 14:58:35 +0100

    transfert

--- src/components/ProfilShowCard/index.jsx
+++ src/components/ProfilShowCard/index.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/fontawesome-free";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons/faCoffee";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const ProfilShowCard = () => {
   const user = useSelector((state) => state.userCreate.user);
-
-  const element = <FontAwesomeIcon icon={faCoffee} />;
-
+  console.log(faCoffee)
   return (
     <>
+      <FontAwesomeIcon icon={faCoffee} />
       <Card>
         <Card.Header>
           <Card.Title>My profile</Card.Title>
